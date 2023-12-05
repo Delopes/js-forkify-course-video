8b822b67b157165314a8e38bf20cad791d4af2a3
Delopes <marianavidigal524@hotmail.com>
2023-12-05 21:46:21 +0000

    deleted newFeature

--- src/js/controller.js
+++ src/js/controller.js
@@ -86,10 +86,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -98,6 +94,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
